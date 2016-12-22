56a45dfde88951db8b3f9a35b7696940011cdfce
liangyang <1935990239@qq.com>
2016-12-22 16:55:29 +0800

    zXing二维码

---
+++ zxingdemo/src/main/java/com/liangyang/zxingdemo/MainActivity.java
@@ -0,0 +1,26 @@
+package com.liangyang.zxingdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.liangyang.zxingdemo.zxing.activity.CaptureActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button mScanBtn = (Button) findViewById(R.id.scan_btn);
+        mScanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
